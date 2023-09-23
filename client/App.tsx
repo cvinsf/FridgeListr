3282b1d8a9771e0bbe4c40b354d6e62545ce2ea2
Carlos Villarreal <lquilpensl@gmail.com>
2023-09-23 23:55:50 +0000

    update index

--- client/App.tsx
+++ client/App.tsx
@@ -1,30 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './index.scss';
 
-import Home from './components/Home.jsx';
-import Login from './components/Login.jsx';
-import Signup from './components/Signup.jsx';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 import ButtonAppBar from './components/ButtonAppBar';
 
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
+interface LoginProps {
+    username: string;
+    setUsername: React.Dispatch<React.SetStateAction<string>>;
+    setUser_id: React.Dispatch<React.SetStateAction<string>>;
+}
 
-function App() {
-    const [username, setUsername] = useState('');
-    const [user_id, setUser_id] = useState('');
-    const [user, setUser] = useState(null);
+interface SignupProps {
+    username: string;
+    setUsername: React.Dispatch<React.SetStateAction<string>>;
+}
 
+interface HomeProps {
+    user_id: string;
+}
+
+const App: React.FC = () => {
+    const [username, setUsername] = useState<string>("");
+    const [user_id, setUser_id] = useState<string>("");
+    const [user, setUser] = useState<object | null>(null);
 
     return (
         <Router>
             <Routes>
-                <Route exact path='/login' element={<Login username={username} setUsername={setUsername} setUser_id={setUser_id}/>} />
-                <Route exact path='/signup' element={<Signup username={username} setUsername={setUsername} />} />
-                <Route exact path='/home' element={<Home user_id={user_id} />} />
+                <Route  path='/login' element={<Login username={username} setUsername={setUsername} setUser_id={setUser_id}/>} />
+                <Route  path='/signup' element={<Signup username={username} setUsername={setUsername} />} />
+                <Route  path='/home' element={<Home user_id={user_id} />} />
             </Routes>
         </Router>
     )
+
 }
 
 
